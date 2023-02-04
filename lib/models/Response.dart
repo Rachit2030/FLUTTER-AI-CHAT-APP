8e039fd7e7ead3d8b67d7c7255a6fd4fe0843697
Rachit2030 <69667845+Rachit2030@users.noreply.github.com>
2023-02-04 18:26:05 +0530

    Image Call Implementation

--- lib/models/Response.dart
+++ lib/models/Response.dart
@@ -18,44 +18,86 @@
 //
 //     final response = responseFromJson(jsonString);
 
-import 'dart:convert';
+// import 'dart:convert';
 
-Response responseFromJson(String str) => Response.fromJson(json.decode(str));
+// Response responseFromJson(String str) => Response.fromJson(json.decode(str));
 
-String responseToJson(Response data) => json.encode(data.toJson());
+// String responseToJson(Response data) => json.encode(data.toJson());
+
+// class Response {
+//   Response({
+//     required this.created,
+//     required this.data,
+//   });
+
+//   int created;
+//   List<Datum> data;
+
+//   factory Response.fromJson(Map<String, dynamic> json) => Response(
+//         created: json["created"],
+//         data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
+//       );
+
+//   Map<String, dynamic> toJson() => {
+//         "created": created,
+//         "data": List<dynamic>.from(data.map((x) => x.toJson())),
+//       };
+// }
+
+// class Datum {
+//   Datum({
+//     required this.url,
+//   });
+
+//   String url;
+
+//   factory Datum.fromJson(Map<String, dynamic> json) => Datum(
+//         url: json["url"],
+//       );
+
+//   Map<String, dynamic> toJson() => {
+//         "url": url,
+//       };
+// }
 
 class Response {
-  Response({
-    required this.created,
-    required this.data,
-  });
+  int? created;
+  List<Data>? data;
 
-  int created;
-  List<Datum> data;
+  Response({this.created, this.data});
 
-  factory Response.fromJson(Map<String, dynamic> json) => Response(
-        created: json["created"],
-        data: List<Datum>.from(json["data"].map((x) => Datum.fromJson(x))),
-      );
+  Response.fromJson(Map<String, dynamic> json) {
+    created = json['created'];
+    if (json['data'] != null) {
+      data = <Data>[];
+      json['data'].forEach((v) {
+        data!.add(new Data.fromJson(v));
+      });
+    }
+  }
 
-  Map<String, dynamic> toJson() => {
-        "created": created,
-        "data": List<dynamic>.from(data.map((x) => x.toJson())),
-      };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['created'] = this.created;
+    if (this.data != null) {
+      data['data'] = this.data!.map((v) => v.toJson()).toList();
+    }
+    return data;
+  }
 }
 
-class Datum {
-  Datum({
-    required this.url,
-  });
+class Data {
+  String? url;
 
-  String url;
+  Data({this.url});
 
-  factory Datum.fromJson(Map<String, dynamic> json) => Datum(
-        url: json["url"],
-      );
+  Data.fromJson(Map<String, dynamic> json) {
+    url = json['url'];
+  }
 
-  Map<String, dynamic> toJson() => {
-        "url": url,
-      };
+  Map<String, dynamic> toJson() {
+    final Map<String, dynamic> data = new Map<String, dynamic>();
+    data['url'] = this.url;
+    return data;
+  }
 }
